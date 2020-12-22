e29928689db48c6f9d719d34415a8f650d0a9b9f
Bleumont <fede.inch@gmail.com>
2020-12-21 23:31:53 -0300

    base app complete

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -20,9 +20,45 @@ class Todo(db.Model):
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        pass
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Hubo un error'
+
     else:
-        return render_template('index.html')
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'Error borrando la entrada'
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task_update = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task_update.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'Error editando la entrada'
+        
+    else:
+        return render_template('update.html', task=task_update)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
